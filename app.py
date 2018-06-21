2cabbce62ae1d48617025da266302f1d909e47b5
SaWLeaDeR <fatihzekisi79@gmail.com>
2018-06-21 12:18:16 +0300

    initial

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
-# coding=utf-8
+#encoding=utf-8
 import os
 import sys
 import json
 from datetime import datetime
-from subprocess 
 
 import requests
 from flask import Flask, request
 
 import pandas as pd
+reload(sys)     
+sys.setdefaultencoding("utf-8")
 
-subprocess.Popen(['Rscript', '/app/webmining.R'],shell=True)
 
-
-data = pd.read_csv("/app/data.headlines.csv",encoding = "utf-8")
-titles = data.iloc[:,0]
-links = data.iloc[:,1].values
-images = data.iloc[:,2].values
+data_sites = ["https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.headlines.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.economy.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.health.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.mag.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.scienceandtech.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.sport.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.turkey.csv",
+         "https://raw.githubusercontent.com/omerfdemir/webmining-chatbot/master/data.world.csv"]
+data_names = ["headlines","economy","health","mag","scienceandtech","sport","turkey","world"]
+j = 0
+for i in data_sites:
+        
+        temp = "data_"+data_names[j]
+        vars()[temp] = pd.read_csv(i)
+        j += 1
+        
+        
+for j in data_names:
+    temp = "data_"+str(j)
+    temp2 = "titles_"+str(j)
+    temp3 = "links_"+str(j)
+    temp4 = "images_"+str(j)
+    vars()[temp2] = vars()[temp].iloc[:,0]
+    vars()[temp3] = vars()[temp].iloc[:,1].values
+    vars()[temp4] = vars()[temp].iloc[:,2].values
+    
 
 
 
@@ -53,21 +74,175 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    jsonData = {"elements":[]}
-                    for i in range(3,10):
 
-                        temp = {
-                            "title": titles[i],
-                            "subtitle": "Element #1 of an hscroll",
-                            "image_url":images[i],
-                            "buttons": [{
-                            "type": "web_url",
-                            "url": links[i],
-                            "title": "web url"}]
-                        }
-                        jsonData["elements"].append(temp)
                     
-                    send_message(sender_id,jsonData["elements"])
+                   # if i in message_text:
+                    if "sport" in message_text or "spor" in message_text or "sports" in message_text or "sporu" in message_text or "spordaki" in message_text or "spordakiler" in message_text:
+                        titles = titles_sport
+                        links = links_sport
+                        images = images_sport
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+                            
+
+                    elif "ekonomi" in message_text or "economy" in message_text or "economies" in message_text or "ekonomidekiler" in message_text or "ekonomidekileri" in message_text:
+                        titles = titles_economy
+                        links = links_economy
+                        images = images_economy
+                        jsonData = {"elements": []}
+                    
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "gündem" in message_text or "headlines" in message_text or "son dakika" in message_text or "popular" in message_text or "gündemi" in message_text or "gündemdekileri" in message_text:
+                        titles = titles_headlines
+                        links = links_headlines
+                        images = images_headlines
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "health" in message_text or "sağlık" in message_text or "sağlıktakiler" in message_text or "sağlıktakileri" in message_text:
+                        titles = titles_health
+                        links = links_health
+                        images = images_health
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "magazin" in message_text or "mag" in message_text or "magazindekiler" in message_text or "magazindekileri" in message_text or "magazini" in message_text:
+                        titles = titles_mag
+                        links = links_mag
+                        images = images_mag
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "bilim" in message_text or "teknoloji" in message_text or "teknolojidekiler" in message_text or "bilimi" in message_text or "technology" in message_text or "science" in message_text or "teknolojidekileri" in message_text or "bilimdekiler" in message_text or "bilimdekileri" in message_text or "bilimdekileri" in message_text:
+                        titles = titles_scienceandtech
+                        links = links_scienceandtech
+                        images = images_scienceandtech
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "turkey" in message_text or "turk" in message_text or "turkiye" in message_text or "turkiyedeki" in message_text or "turkiyedekiler" in message_text:
+                        titles = titles_turkey
+                        links = links_turkey
+                        images = images_turkey
+                        jsonData = {"elements": []}
+
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                        send_card(sender_id,jsonData["elements"])
+
+                    elif "dunya" in message_text or "world" in message_text or "dunyadaki" in message_text or "dunyadakiler" in message_text or "dunyadakileri" in message_text:
+                        titles = titles_world
+                        links = links_world
+                        images = images_world
+                        jsonData = {"elements":[]}
+
+                        
+                        for i in range(0,10):
+
+                            temp = {
+                                "title": titles[i],
+                                "subtitle": "",
+                                "image_url":images[i],
+                                "buttons": [{
+                                "type": "web_url",
+                                "url": links[i],
+                                "title": "See More"}]
+                            }
+                            jsonData["elements"].append(temp)
+                                
+                        send_card(sender_id,jsonData["elements"])
+                    else:
+                        send_message(sender_id,"Hello")
+                            
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -81,7 +256,7 @@ def webhook():
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
+def send_card(recipient_id, message_text):
 
     #log("sending message to "+ str(recipient_id) + message_text)
 
@@ -111,14 +286,35 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def send_message(recipient_id, message_text):
 
+    #log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
     try:
         if type(msg) is dict:
             msg = json.dumps(msg)
         else:
-            msg = msg.format(*args, **kwargs)
+            msg = str(msg)
         print (msg)
     except UnicodeEncodeError:
         pass  # squash logging errors in case of non-ascii text
--- webminingproject.R
+++ webminingproject.R
@@ -1,7 +1,16 @@
-library(rvest)
-library("XML")
+
+library("rvest")
 library("httr")
-setwd("/app/")
+library(rstudioapi)
+options(encoding = "UTF-8")
+
+# the following line is for getting the path of your current open file
+current_path <- getActiveDocumentContext()$path 
+# The next line set the working directory to the relevant one:
+setwd(dirname(current_path ))
+# you can make sure you are in the right directory
+print( getwd() )
+
 
 websites <- c("https://news.google.com/news/?ned=tr_tr&gl=TR&hl=tr",
               "https://news.google.com/news/headlines/section/topic/WORLD.tr_tr/D%C3%BCnya?ned=tr_tr&hl=tr&gl=TR",
@@ -17,24 +26,49 @@ websites <- c("https://news.google.com/news/?ned=tr_tr&gl=TR&hl=tr",
 getData <- function(website) {
   data <- data.frame()
   webpage <- read_html(website)
-  data <- html_nodes(webpage,".kWyHVd .ME7ew")
-  data2 <- html_nodes(webpage,".lmFAjc , .kWyHVd .ME7ew")
-  img_data <- html_nodes(webpage,".lmFAjc")
-  img_data <- html_attr(img_data,"src")
-  data2_img <- html_attr(data2,"src")
+  data <- html_nodes(webpage,".Cc0Z5d .VDXfz")
+  data_2 <- html_nodes(webpage,".Cc0Z5d  , .dIH98c")
+  data2_title <- html_text(data_2)
+  data2_title[data2_title == ""] <- NA
+  data2_img <- html_attr(data_2,"src")
   data2_img <- c(NA,data2_img) 
   data2_img <- data2_img[1:(length(data2_img)-1)]
-  #data2_img <- na.omit(data2_img)
-  data2_title <- html_text(data2)
-  #data2_title <- data2_title[data2_title != ""]
-  data2_link <-  html_attr(data2,"href")
-  #data2_link <- na.omit(data2_link)
+  
+  
+  for (i in 1:length(data2_title)) {
+    if (is.na(data2_title[i])) {
+      data2_title <- data2_title[-c(i)]
+      data2_img <- data2_img[-c(i)]
+    }
+  }
+  
+  data2_link <-  html_attr(data,"href")
+  
+  data2_link <- gsub("^./","" , data2_link)
+  data2_link <- paste("https://news.google.com/", data2_link , sep="" )
   data2.df <- data.frame(data2_title,data2_link,data2_img)
   names(data2.df) <- c("title","link","img")
   data3.df <- data2.df[(data2.df$title != ""),]
+  data3.df[,3] <- as.character(data3.df[,3])
+  sayi <- nrow(data3.df)
+  for (i in 1:sayi) {
+    if (is.na(data3.df[i,3])) {
+      print('here')
+      data3.df[i,3] <- 'https://drive.google.com/file/d/1o_a7qyvWlg2c8zJ9S1SMSADxnWlJ2B0T/view?usp=sharing'
+      
+    }
+    else {
+      print('end')
+    }
+    
+  }
   return (data3.df)
   
 }
+  
+
+
+
 
 
 i <- 1
